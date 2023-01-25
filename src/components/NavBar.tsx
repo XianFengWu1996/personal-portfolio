e23a71e34b088757e15ae46b91e31dd3ba4d7639
XianFengWu1996 <46692996+XianFengWu1996@users.noreply.github.com>
2023-01-25 14:42:43 -0500

    include logo asset and set up default fonts

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import { Electrolize } from '@next/font/google';
+
+const electrolize = Electrolize({
+  weight: ['400'],
+  subsets: ['latin'],
+});
+
+const NavBar = () => {
+  return <header></header>;
+};
+
+export default NavBar;
